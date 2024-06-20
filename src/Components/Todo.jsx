2a581fc9cbb16927eeb26d61c3b88535be510019
anurag050301 <anurag050301@users.noreply.github.com>
2024-06-20 21:53:24 +0530

    added functionality to store data in local storage to prevent the data loss if page reloaded or browser reopened.

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,27 +1,86 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { FaRegCalendarPlus } from "react-icons/fa6";
 import TodoItems from "./TodoItems";
 const Todo = () => {
+  const [todoList, setTodoList] = useState(localStorage.getItem("todos")?JSON.parse(localStorage.getItem("todos")):[]);
+  const inputRef = useRef();
+  const getCurrentDate = () => {
+    const date = new Date();
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString(undefined, options);
+  };
+  const addData = () => {
+    const inputText = inputRef.current.value.trim();
+    if (inputText === "") {
+      return;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
+  const deleteData = (id) => {
+    setTodoList((prevTodo) => {
+      return prevTodo.filter((todo) => todo.id !== id);
+    });
+  };
+  const toggle = (id) => {
+    setTodoList((prevToDo) => {
+      return prevToDo.map((todo) => {
+        if (todo.id === id) return { ...todo, isComplete: !todo.isComplete };
+        return todo;
+      });
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoList));
+  }, [todoList]);
+
   return (
-    <div className="bg-zinc-100 place-self-center w-11/12 max-w-lg flex flex-col p-7 min-h-[550px] rounded-xl shadow-xl">
+    <div className="bg-zinc-100 place-self-center w-11/12 max-w-lg flex flex-col p-7 min-h-[300px] rounded-xl shadow-xl">
+      {/* -------title------- */}
 
-    {/* -------title------- */}
-
-    <div className="flex items-center mt-7 gap-1.5">
-        <FaRegCalendarPlus className="size-8 pb-0.5"/>
+      <div className="flex items-center  gap-1.5">
+        <FaRegCalendarPlus className="size-8 pb-0.5" />
         <h1 className="text-3xl font-semibold">To-Do List</h1>
-    </div>
-    {/* -------input box------- */}
-    <div className="flex item-center my-7 bg-gray-200 rounded-full hover:shadow-xl">
-        <input className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600" type = "text" placeholder="Add your task"/>
-        <button className="border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer">Add +</button>
-    </div>
-    {/* -------todo list------- */}
-    <div>
-        <TodoItems text = "Learn Coding"/>
-        <TodoItems text = "Learn DSA"/>
-        <TodoItems text = "Learn C++"/>
-    </div>
+        <span className="ml-[110px] text-lg text-gray-600">
+          Date: {getCurrentDate()}
+        </span>
+      </div>
+      {/* -------input box------- */}
+      <div className="flex item-center my-7 bg-gray-200 rounded-full hover:shadow-xl ease-in-out duration-300">
+        <input
+          ref={inputRef}
+          className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
+          type="text"
+          placeholder="Add your task"
+        />
+        <button
+          className="border-none rounded-full bg-orange-600 w-[90px] h-14 text-white text-lg font-medium cursor-pointer"
+          onClick={addData}
+        >
+          Add +
+        </button>
+      </div>
+      {/* -------todo list------- */}
+      <div>
+        {todoList.map((item, index) => {
+          return (
+            <TodoItems
+              key={index}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+              deleteData={deleteData}
+              toggle={toggle}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-import { FaCircleCheck, FaTrashCan } from 'react-icons/fa6'
-const TodoItems = ({text}) => {
+import React from "react";
+import { FaCircleCheck, FaTrashCan, FaRegCircle } from "react-icons/fa6";
+const TodoItems = ({ text, id, isComplete, deleteData, toggle }) => {
+  const ans = isComplete ? (
+    <FaCircleCheck className="text-orange-600 text-2xl w-7 cursor-pointer" />
+  ) : (
+    <FaRegCircle className="text-slate-600 text-2xl w-7 cursor-pointer" />
+  );
   return (
-    <div className='flex items-center my-3 gap-2'>
-        <div className='flex flex-1'>
-        <FaCircleCheck className='text-orange-600 text-2xl w-7'/>
-        <p className='text-slate-800 ml-4 text-[17px]'>{text}</p>
-        </div>
-        <FaTrashCan className='w-3.5 cursor-pointer text-slate-600 hover:text-rose-400'/>
+    <div
+      onClick={() => {
+        toggle(id);
+      }}
+      className="flex items-center h-12 gap-2 px-4 hover:bg-zinc-300 rounded-2xl ease-in-out duration-300"
+    >
+      <div className="flex flex-1">
+        {ans}
+        <p
+          className={`text-slate-800 ml-4 text-[17px] decoration-slate-500 ${
+            isComplete ? "line-through" : ""
+          }`}
+        >
+          {text}
+        </p>
+      </div>
+      <FaTrashCan
+        className="w-3.5 cursor-pointer text-slate-600 hover:text-rose-400"
+        onClick={() => {
+          deleteData(id);
+        }}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TodoItems
+export default TodoItems;
